21d43b328f3e99a699c18fead4261e0ad8c7208f
LekizRai <nguyenvuhoangtruong@gmail.com>
2026-05-09 11:15:26 +0200

    Add 09/05/2026 solutions

--- dynamic-programming/minimal-grid-path.cpp
+++ dynamic-programming/minimal-grid-path.cpp
@@ -5,28 +5,51 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    string s;
+    int n, flag;
+    char min_char;
     cin >> n;
-    vector<vector<string>> res(2, vector<string> (n));
-    for (int i = 0; i < min(n, 1500); i++) {
-        cin >> s;
-        for (int j = 0; j < min(n, 1500); j++) {
-            if (i == 0 && j == 0) {
-                res[i][j] = s[j];
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    string res = "";
+    res += s[0][0];
+    vector<pair<int, int>>  dig[2];
+    flag = 0;
+    dig[flag].push_back(pair<int, int> (0, 0));
+    map<pair<int, int>, bool> exist;
+    for (int i = 1; i <= min((n-1)*2-1, 4000); i++) {
+        min_char = 'a';
+        for (auto &it: dig[flag]) {
+            exist[pair<int, int> (it.first+1, it.second)] = false;
+            exist[pair<int, int> (it.first, it.second+1)] = false;
+            if (it.first+1 < n) min_char = min(min_char, s[it.first+1][it.second]);
+            if (it.second+1 < n) min_char = min(min_char, s[it.first][it.second+1]);
+        }
+        dig[(flag+1)%2].clear();
+        for (auto &it: dig[flag]) {
+            if (
+                it.first+1 < n 
+                && s[it.first+1][it.second] == min_char
+                && !exist[pair<int, int> (it.first+1, it.second)]
+            ) {
+
+                dig[(flag+1)%2].push_back(pair<int, int> (it.first+1, it.second));
+                exist[pair<int, int> (it.first+1, it.second)] = true;
             }
-            else {
-                res[i%2][j] = "a";
-                if (i-1 >= 0) {
-                    res[i%2][j] = min(res[i%2][j], res[(i+1)%2][j]);
-                }
-                if (j-1 >= 0) {
-                    res[i%2][j] = min(res[i%2][j], res[i%2][j-1]);
-                }
-                res[i%2][j] += s[j];
+            if (
+                it.second+1 < n
+                && s[it.first][it.second+1] == min_char
+                && !exist[pair<int, int> (it.first, it.second+1)]
+            ) {
+                dig[(flag+1)%2].push_back(pair<int, int> (it.first, it.second+1));
+                exist[pair<int, int> (it.first, it.second+1)] = true;
             }
         }
+        flag = (flag+1)%2;
+        res += min_char;
     }
-    cout << res[(n-1)%2][n-1];
+    if (n > 1) res += s[n-1][n-1];
+    cout << res;
     return 0;
 }
